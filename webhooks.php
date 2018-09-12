6d14dff50c078e4f71fff13f07bae6e48080c2a2
Qinfo-inforvation <42992185+Qinfo-inforvation@users.noreply.github.com>
2018-09-12 16:22:41 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -14,8 +14,10 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == 'mid') {
 			// Get text sent
+
+				
 			$text = $event['source']['userId'];
 				//['userId'];
 			// Get replyToken
